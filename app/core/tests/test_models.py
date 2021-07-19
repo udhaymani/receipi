c96d69cf6465e04d596fef79ede80cf71ad7fbaf
udhayakumar <mudhay93@gmail.com>
2021-07-19 21:11:08 +0530

    second commit

---
+++ app/core/tests/test_models.py
@@ -0,0 +1,43 @@
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+
+class ModelTests(TestCase):
+
+    def test_create_user_email_successful(self):
+
+        '''Test creating new user with Email'''
+
+        email = 'test@gmail.com'
+        password="test!123"
+        user = get_user_model().objects.create_user(
+            email=email,
+            password=password
+        )
+
+        self.assertEqual(user.email,email)
+        self.assertTrue(user.check_password(password))
+
+
+    def test_new_user_email_normalized(self):
+        email = "test@gmail.com"
+        user = get_user_model().objects.create_user(
+            email,
+            'test!123'
+        )    
+
+        self.assertEqual(user.email,email.lower())
+
+    def test_new_user_invalid_email(self):
+        """Test creating with no email raises error"""
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user(None,"test123")    
+
+    def test_create_superuser(self):
+        """Test creating new super user"""
+        user = get_user_model().objects.create_superuser(
+            'test@gmail.com',
+            'test123'
+        )        
+
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
